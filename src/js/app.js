f8b6f01e1a21bd00a98c6f4b4c3c322a0da60a04
Serhii <kso_132@mail.ru>
2018-01-12 21:51:42 +0200

    edits in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
-import svgUseIt from 'svg-use-it';
-import scrollMagic from './components/scrollmagic.js';
-import techAnimation from './components/tech-animation.js';
-import slick from 'slick-carousel';
+// import svgUseIt from 'svg-use-it';
+// import scrollMagic from './components/scrollmagic.js';
+// import techAnimation from './components/tech-animation.js';
+// import slick from 'slick-carousel';
 
 $(document).ready(function(){
 
-	svgUseIt();
-	scrollMagic();
-	techAnimation();
+	// svgUseIt();
+	// scrollMagic();
+	// techAnimation();
 
 	//end
 });
